2d9194be7c54b5aea5a6eff3d85454dfbcabcb20
sen <sorrymaker@qq.com>
2022-04-04 15:36:51 +0800

    init project

---
+++ src/main/java/com/lhs/ms/goforit/GoForItApplication.java
@@ -0,0 +1,13 @@
+package com.lhs.ms.goforit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoForItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoForItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhs/pp/goforit/GoForItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhs.pp.goforit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoForItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
